7ccd77e47a6125b590f6e50a35c29799c712d8d0
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-17 16:48:36 +0100

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/home'
+import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import Theme from './Theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
