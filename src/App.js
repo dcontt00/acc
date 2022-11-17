e93631dc4cea0329d8a8e6ca882f3e2909378429
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-17 17:05:54 +0100

    Cambiado nombre Home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import Home from './pages/home'
-import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import Theme from './Theme'
-import Appbar from './components/Appbar'
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import Theme from "./Theme";
+import Appbar from "./components/Appbar";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <Appbar />
 
-
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-
-
       </Router>
     </ThemeProvider>
-
   );
 }
 
