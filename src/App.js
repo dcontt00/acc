0d12e10a0cb4ed2f06ad110eaf57231a1d81041a
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-17 16:50:15 +0100

    Añadida appbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Home from './pages/home'
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import Theme from './Theme'
+import Appbar from './components/Appbar'
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
+      <Appbar />
 
 
       <Router>
