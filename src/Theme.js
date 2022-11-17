3202c382cefd98b51c03fd5fb108377d2bba6250
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-17 17:16:11 +0100

    Añadidos tamaños fuentes

--- src/Theme.js
+++ src/Theme.js
@@ -1,11 +1,40 @@
-import { createTheme } from '@mui/material/styles';
-import { height } from '@mui/system';
+import { createTheme } from "@mui/material/styles";
+import { height } from "@mui/system";
 
+const Theme = createTheme({});
 
-const theme = createTheme({
+Theme.typography.h1 = {
+  fontSize: "2.5rem",
+  paddingBottom: "0.5rem",
+  paddingTop: "0.5rem",
 
+  [Theme.breakpoints.up("md")]: {
+    fontSize: "3rem",
+    paddingBottom: "1rem",
+    paddingTop: "1rem",
+  },
+};
 
+Theme.typography.h2 = {
+  fontSize: "2rem",
+  paddingBottom: "0.1rem",
+  paddingTop: "0.1rem",
 
-});
+  [Theme.breakpoints.up("md")]: {
+    fontSize: "2.5rem",
+    paddingBottom: "0.2rem",
+    paddingTop: "0.2rem",
+  },
+};
 
-export default theme;
+Theme.typography.h3 = {
+  fontSize: "1.5rem",
+
+  [Theme.breakpoints.up("md")]: {
+    fontSize: "2rem",
+    paddingBottom: "0.4rem",
+    paddingTop: "0.4rem",
+  },
+};
+
+export default Theme;
