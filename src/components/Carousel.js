dbbb4c5ccd814a61972fed0203d86e9e5eef978a
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-17 17:05:09 +0100

    Movidos datos de coches a coches.json

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,24 +1,12 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Button } from "@mui/material";
+import Coches from "../coches.json";
 
 export default function Carousel_comp(props) {
-  var items = [
-    {
-      name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
-      image: "coche1.jpg",
-    },
-    {
-      name: "Random Name #2",
-      description: "Hello World!",
-      image: "coche2.jpg",
-    },
-  ];
-
   return (
     <Carousel>
-      {items.map((item, i) => (
+      {Coches.map((item, i) => (
         <Item key={i} item={item} />
       ))}
     </Carousel>
@@ -30,7 +18,6 @@ function Item(props) {
     <Paper>
       <h2>{props.item.name}</h2>
       <p>{props.item.description}</p>
-      {console.log(process.env.PUBLIC_URL)}
       <img src={props.item.image} alt="coche" />
 
       <Button className="CheckButton">Check it out!</Button>
