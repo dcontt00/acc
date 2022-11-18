75844ec160c495c6515f8c093d0cc6e3ac3a6bb6
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-18 12:02:21 +0100

    Creada CarCard

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,18 @@
 import * as React from "react";
 import Cars from "../data/Cars.json";
 import Grid from "@mui/material/Grid";
+import CarCard from "../components/CarCard";
 
 export default function StandardImageList(props) {
   return (
     <Grid container spacing={2}>
       {props.cars.map((coche) => (
         <Grid item xs={12} sm={6} md={6} lg={3}>
-          <img src={coche.img} alt={coche.name} width={"100%"} />
+          <CarCard
+            img={coche.img}
+            title={coche.name}
+            description={coche.description}
+          />
         </Grid>
       ))}
     </Grid>
