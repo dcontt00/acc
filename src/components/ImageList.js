db5ade4cdbb2bdc29be33a2969eb7bc2f81f6e69
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-11-24 10:08:47 +0100

    Cambios

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,47 +1,23 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import { Button } from '@mui/material';
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { Button } from "@mui/material";
 
-
-export default function StandardImageList() {
+export default function StandardImageList(props) {
   return (
     <ImageList cols={5} item xs={12} sm={6} md={4} lg={3}>
-      {itemData.map((item) => (
+      {props.data.map((item) => (
         <Button>
-            <ImageListItem key={item.img}>
+          <ImageListItem key={item.img}>
             <img
-                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
             />
-            </ImageListItem>
+          </ImageListItem>
         </Button>
       ))}
     </ImageList>
   );
 }
-
-const itemData = [
-  {
-    img: 'imgs/llantas/llantas_1.jpg',
-    title: 'Llantas 1',
-  },
-  {
-    img: 'imgs/llantas/llantas_2.jpg',
-    title: 'Llantas 2',
-  },
-  {
-    img: 'imgs/llantas/llantas_3.jpg',
-    title: 'Llantas 3',
-  },
-  {
-    img: 'imgs/llantas/llantas_4.jpg',
-    title: 'Llantas 4',
-  },
-  {
-    img: 'imgs/llantas/llantas_5.jpg',
-    title: 'Llantas 5',
-  }
-];
--- src/pages/Personalize.js
+++ src/pages/Personalize.js
@@ -1,17 +1,61 @@
 import "../App.css";
-import { Button, Grid, Typography} from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import Carousel from "../components/CarouselCar";
 import Container from "@mui/material/Container";
 import ImageList from "../components/ImageList";
-import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
+const itemData = [
+  {
+    img: "imgs/llantas/llantas_1.jpg",
+    title: "Llantas 1",
+  },
+  {
+    img: "imgs/llantas/llantas_2.jpg",
+    title: "Llantas 2",
+  },
+  {
+    img: "imgs/llantas/llantas_3.jpg",
+    title: "Llantas 3",
+  },
+  {
+    img: "imgs/llantas/llantas_4.jpg",
+    title: "Llantas 4",
+  },
+  {
+    img: "imgs/llantas/llantas_5.jpg",
+    title: "Llantas 5",
+  },
+];
 
+const itemData2 = [
+  {
+    img: "imgs/cars/audi_a1_1.jpg",
+    title: "Llantas 1",
+  },
+  {
+    img: "imgs/llantas/llantas_2.jpg",
+    title: "Llantas 2",
+  },
+  {
+    img: "imgs/llantas/llantas_3.jpg",
+    title: "Llantas 3",
+  },
+  {
+    img: "imgs/llantas/llantas_4.jpg",
+    title: "Llantas 4",
+  },
+  {
+    img: "imgs/llantas/llantas_5.jpg",
+    title: "Llantas 5",
+  },
+];
 export default function Personalize() {
   return (
     <Container maxWidth="lg">
@@ -29,7 +73,7 @@ export default function Personalize() {
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
-            <ImageList />
+          <ImageList data={itemData2} />
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -37,7 +81,7 @@ export default function Personalize() {
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
-            <ImageList />
+          <ImageList data={itemData} />
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -45,26 +89,40 @@ export default function Personalize() {
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
-            <ImageList />
+          <ImageList data={itemData} />
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
           <Typography variant="h3">Motor</Typography>
         </Grid>
 
-        <Grid item xs={12} sm={12} md={12} lg={12}> 
-            <FormControl>
-                <FormLabel id="demo-radio-buttons-group-label">Tipo de motor</FormLabel>
-                <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="hybrid"
-                name="radio-buttons-group"
-                >
-                    <FormControlLabel value="hybrid" control={<Radio />} label="Híbrido" />
-                    <FormControlLabel value="gas" control={<Radio />} label="Gasolina" />
-                    <FormControlLabel value="diesel" control={<Radio />} label="Diesel" />
-                </RadioGroup>
-            </FormControl>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">
+              Tipo de motor
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="hybrid"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="hybrid"
+                control={<Radio />}
+                label="Híbrido"
+              />
+              <FormControlLabel
+                value="gas"
+                control={<Radio />}
+                label="Gasolina"
+              />
+              <FormControlLabel
+                value="diesel"
+                control={<Radio />}
+                label="Diesel"
+              />
+            </RadioGroup>
+          </FormControl>
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -72,16 +130,25 @@ export default function Personalize() {
         </Grid>
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
-            <ImageList />
+          <ImageList data={itemData} />
         </Grid>
 
-        <Typography variant="h4" item xs={8} sm={8} md={8} lg={8} padding={5}> Precio: 25.000$</Typography>
+        <Typography variant="h4" item xs={8} sm={8} md={8} lg={8} padding={5}>
+          {" "}
+          Precio: 25.000$
+        </Typography>
 
-        <Button variant="contained" item xs={4} sm={4} md={4} lg={4} padding={5}>
-            Comprar <ShoppingCartIcon />
+        <Button
+          variant="contained"
+          item
+          xs={4}
+          sm={4}
+          md={4}
+          lg={4}
+          padding={5}
+        >
+          Comprar <ShoppingCartIcon />
         </Button>
-        
-
       </Grid>
     </Container>
   );
