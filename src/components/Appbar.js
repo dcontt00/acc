af0099e39d1c81c39babd319a5139bd3cc606afa
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-12-02 10:40:19 +0100

    Modificada barra busqueda

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,21 +1,11 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+import { MenuItem, AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, InputBase } from '@mui/material'
 import AdbIcon from "@mui/icons-material/Adb";
 import { useNavigate } from "react-router-dom";
-import { Divider, InputBase, TextField, Paper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import DirectionsIcon from "@mui/icons-material/Directions";
+import { styled, alpha } from '@mui/material/styles';
 import Cookie from "universal-cookie";
 const cookie = new Cookie();
 
@@ -32,6 +22,50 @@ const settings = [
   "Dashboard",
   "Logout",
 ];
+
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 function ResponsiveAppBar() {
   const navigate = useNavigate();
@@ -185,20 +219,19 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <Paper
-            component="form"
-            sx={{ display: 'flex', alignItems: 'center', width: 400 }}
-          >
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              size="small"
-              placeholder="Buscar un coche"
-              inputProps={{ 'aria-label': 'search google maps' }}
-            />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+
+          <Search>
+            <SearchIconWrapper>
               <SearchIcon />
-            </IconButton>
-          </Paper>
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Buscar..."
+              inputProps={{ 'aria-label': 'search' }}
+
+            />
+          </Search>
+
+
           {/*Hide menu if user is not loged*/}
           {loged ? (
             <Box sx={{ flexGrow: 0 }}>
