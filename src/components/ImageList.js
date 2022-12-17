0b04447d81b7a0d2153f57d216784fcc393001bc
ynikor00 <ynikor00@estudiantes.unileon.es>
2022-12-17 17:58:01 +0100

    Seleccion de personalizacion solucionada

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,24 +2,47 @@ import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { List, ListItemButton, Button } from "@mui/material";
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import { styled } from "@mui/material/styles";
+
+const StyledList = styled(List)({
+  // selected and (selected + hover) states
+  "&& .Mui-selected, && .Mui-selected:hover": {
+    backgroundColor: "red",
+    "&, & .MuiListItemIcon-root": {
+      color: "pink",
+    },
+  },
+  // hover states
+  "& .MuiListItemButton-root:hover": {
+    backgroundColor: "orange",
+    "&, & .MuiListItemIcon-root": {
+      color: "yellow",
+    },
+  },
+});
 
 export default function StandardImageList(props) {
-  const [selected, setSelected] = React.useState(0);
+  const [selectedIndex, setSelected] = React.useState(0);
 
-  const handleListItemClick = (event,index) => {
+  const handleListItemClick = (event, index) => {
     setSelected(index);
   };
 
   return (
-    <List sx={{display: "flex", flexDirection: "row"}} xs={12} sm={6} md={4} lg={3}>
+    <StyledList
+      sx={{ display: "flex", flexDirection: "row" }}
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+    >
       {props.data.map((item, key) => (
         <ListItemButton
-          selected ={selected === key}
-          sx={{ backgroundColor: key === selected ? "blue" : "transparent" }}
-          
+          /*sx={{ backgroundColor: key === selected ? "blue" : "transparent" }}*/
+          selected={selectedIndex === key}
           onClick={(ev) => handleListItemClick(ev, key)}
         >
           <ImageListItem key={item.img}>
@@ -35,7 +58,7 @@ export default function StandardImageList(props) {
             subtitle={item.price}
             actionIcon={
               <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
                 aria-label={`info about ${item.title}`}
               >
                 <InfoIcon />
@@ -44,6 +67,6 @@ export default function StandardImageList(props) {
           />
         </ListItemButton>
       ))}
-    </List>
+    </StyledList>
   );
 }
