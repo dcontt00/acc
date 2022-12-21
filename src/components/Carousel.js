4a46e035db3bd335de743014e3a2b510b0af628e
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-12-21 17:58:53 +0100

    Cambiado carrousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,37 +1,62 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Paper } from "@mui/material";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useNavigate } from "react-router-dom";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 export default function Carousel_comp(props) {
   const navigate = useNavigate();
 
   return (
-    <Carousel autoPlay showArrows>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      loop={true}
+      autoplay={{ delay: 3000 }}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+
       {props.cars.map((item, i) => (
-        <div key={i} onClick={() => navigate("description/" + item.id)}>
+
+        <SwiperSlide onClick={() => navigate("description/" + item.id)}>
           <Item key={i} item={item} />
-        </div>
+        </SwiperSlide>
       ))}
-    </Carousel>
+    </Swiper>
   );
 }
 
 function Item(props) {
   return (
-    <Grid container>
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        <Typography variant="h2">{props.item.name}</Typography>
+    <Paper sx={{ p: 2 }}>
+
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Typography variant="h2">{props.item.name}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Typography variant="p">{props.item.description}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <img src={process.env.PUBLIC_URL + props.item.img} alt="coche" width={"100%"} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12}></Grid>
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        <Typography variant="p">{props.item.description}</Typography>
-      </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        <img src={process.env.PUBLIC_URL + props.item.img} alt="coche" width={"70%"} />
-      </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12}></Grid>
-    </Grid>
+    </Paper>
   );
 }
