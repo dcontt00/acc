9409bddae0b32db2c28e24f363a2c1019201301d
dcontt00 <dcontt00@estudiantes.unileon.es>
2023-01-09 15:36:26 +0100

    Cambiado boton mas para que entre el precio

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -148,7 +148,7 @@ export default function MultiActionAreaCard(props) {
       </CardActionArea>
       <CardActions>
         <Grid container spacing={2}>
-          <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Grid item xs={5} sm={5} md={5} lg={5}>
             <Typography variant="h6" color="text.secondary">
               {price}
             </Typography>
@@ -177,9 +177,9 @@ export default function MultiActionAreaCard(props) {
             message={snackbarMessage}
             action={action}
           />
-          <Grid item xs={5} sm={5} md={5} lg={5}>
+          <Grid item xs={4} sm={4} md={4} lg={4}>
             <AButton
-              text="Saber más"
+              text="Más"
               sx={{ width: "100%" }}
               onClick={() => navigate("/description/" + props.car.id)}
             />
