f52d4c77073db08d45ebe61154b462109dbfae6d
dcontt00 <dcontt00@estudiantes.unileon.es>
2023-01-11 18:41:53 +0100

    Solucionado que se muestre la parte de debajo de la pagina

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from "./ScrollToTop";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
