d3a1f076468a2f9eb95709805abe2fd1419a28b1
dcontt00 <dcontt00@estudiantes.unielon.es>
2023-01-11 21:38:54 +0100

    cambiado color

--- src/components/StyledList.js
+++ src/components/StyledList.js
@@ -1,20 +1,26 @@
 
 import { styled } from "@mui/material/styles";
-import {List} from "@mui/material"
+import { List } from "@mui/material"
+import Theme from "../Theme"
+
+var color = Theme.palette.primary.main
 
 export const StyledList = styled(List)({
-    // selected and (selected + hover) states
-    "&& .Mui-selected, && .Mui-selected:hover": {
-      border: "5px solid #000",
-      "&, & .MuiListItemIcon-root": {
-        color: "pink",
-      },
+
+  // selected and (selected + hover) states
+  "&& .Mui-selected, && .Mui-selected:hover": {
+    borderStyle: "solid",
+    borderWidth: "5px",
+    borderColor: "#0A2647",
+    "&, & .MuiListItemIcon-root": {
+      color: "pink",
     },
-    // hover states
-    "& .MuiListItemButton-root:hover": {
-      backgroundColor: "#cccccc",
-      "&, & .MuiListItemIcon-root": {
-        color: "yellow",
-      },
+  },
+  // hover states
+  "& .MuiListItemButton-root:hover": {
+    backgroundColor: "#cccccc",
+    "&, & .MuiListItemIcon-root": {
+      color: "yellow",
     },
-  });
+  },
+});
