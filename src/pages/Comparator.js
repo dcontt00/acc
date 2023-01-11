7e20add8cd59f4e886d9bd753c61a08a890209d1
dcontt00 <dcontt00@estudiantes.unileon.es>
2023-01-11 18:11:23 +0100

    cambiado layout

--- src/pages/Comparator.js
+++ src/pages/Comparator.js
@@ -21,6 +21,7 @@ import Cookie from "universal-cookie";
 import AddIcon from "@mui/icons-material/Add";
 import CompareModal from "../components/CompareModal";
 import EditIcon from '@mui/icons-material/Edit';
+import { Container } from "@mui/system";
 
 const cookie = new Cookie();
 function DescriptionList(props) {
@@ -105,91 +106,27 @@ export default function Comparator(props) {
   };
 
   return (
-    <Grid container sx={{ padding: 5 }}>
-      <Grid item xs={12}>
-        <Typography variant="h2">Comparador</Typography>
-      </Grid>
-      <Divider />
-      <Grid container spacing={4} sx={{ marginBottom: "5%", marginTop:"5px" }}>
-        <Grid item xs={6}>
-          {/*First Car image*/}
-          {firstCar ? (
-            <Card sx={{ width: "100%", height: "100%", textAlign: "center", position:"relative" }} onClick={() => handleOpen(true)}>
-              <Fab sx={{position: "absolute", top: "25px", right: "25px"}}>
-                <EditIcon fontSize="large"/>
-              </Fab>
-              <CardContent>
-                <Grid container spacing={3}>
-                  <Grid item xs={12}>
-                    <img
-                      src={process.env.PUBLIC_URL + firstCar.img}
-                      alt="First car"
-                      style={{
-                        objectFit: "cover",
-                        objectPosition: "center",
-                        width: "100%",
-                        height: "auto",
-                      }}
-                    />
-                  </Grid>
-                  <Grid item xs={2} sm={2} md={2} lg={2}>
-                    <img
-                      src={getBrandImg(firstCar.brand)}
-                      alt="brand"
-                      style={{
-                        objectFit: "cover",
-                        objectPosition: "center",
-                        width: "80%",
-                        height: "auto",
-                      }}
-                    />
-                  </Grid>
-                  <Grid item xs={5}>
-                    <Typography
-                      gutterBottom
-                      variant="h3"
-                      align="left"
-                      component="div"
-                    >
-                      {firstCar.name}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={5}>
-                    <Typography align="right" variant="h3">
-                      {firstCar.year}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography align="left" variant="h5" color="">
-                      {firstCar.description}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          ) : (
-            <Button onClick={() => handleOpen(true)} variant="contained" sx={{width:"100%", minHeight:"500px"}}>
-              <Grid container>
-                <Grid item align="center" xs={12}>
-                  <AddIcon fontSize="large"></AddIcon>
-                </Grid>
-              </Grid>
-            </Button>
-          )}
+    <Container maxWidth="xl">
+
+      <Grid container sx={{ padding: 5 }}>
+        <Grid item xs={12}>
+          <Typography variant="h2">Comparador</Typography>
+          <Typography variant="body1">Elige dos coches para ver y comparar sus características</Typography>
         </Grid>
-        <Grid item xs={6}>
-          {/*Second Car image*/}
-          {secondCar ? (
-            <Card sx={{ width: "100%", height: "100%", textAlign: "center", position: "relative"}} onClick={() => handleOpen(false)}>
-              <Fab sx={{position: "absolute", top: "25px", right: "25px"}}>
-                <EditIcon fontSize="large"/>
-              </Fab>
-              <CardActionArea>
+        <Divider />
+        <Grid container spacing={4} sx={{ marginBottom: "5%", marginTop: "5px" }}>
+          <Grid item xs={6}>
+            {/*First Car image*/}
+            {firstCar ? (
+              <Card sx={{ width: "100%", height: "100%", textAlign: "center", position: "relative" }} onClick={() => handleOpen(true)}>
+                <Fab sx={{ position: "absolute", top: "25px", right: "25px" }}>
+                  <EditIcon fontSize="large" />
+                </Fab>
                 <CardContent>
                   <Grid container spacing={3}>
                     <Grid item xs={12}>
                       <img
-                        src={process.env.PUBLIC_URL + secondCar.img}
+                        src={process.env.PUBLIC_URL + firstCar.img}
                         alt="First car"
                         style={{
                           objectFit: "cover",
@@ -201,7 +138,7 @@ export default function Comparator(props) {
                     </Grid>
                     <Grid item xs={2} sm={2} md={2} lg={2}>
                       <img
-                        src={getBrandImg(secondCar.brand)}
+                        src={getBrandImg(firstCar.brand)}
                         alt="brand"
                         style={{
                           objectFit: "cover",
@@ -218,60 +155,132 @@ export default function Comparator(props) {
                         align="left"
                         component="div"
                       >
-                        {secondCar.name}
+                        {firstCar.name}
                       </Typography>
                     </Grid>
                     <Grid item xs={5}>
                       <Typography align="right" variant="h3">
-                        {secondCar.year}
+                        {firstCar.year}
                       </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                      <Typography align="left" variant="h5">
-                        {secondCar.description}
+                      <Typography align="left" variant="h5" color="">
+                        {firstCar.description}
                       </Typography>
                     </Grid>
                   </Grid>
                 </CardContent>
-              </CardActionArea>
-            </Card>
-          ) : (
-            <Button onClick={() => handleOpen(false)} variant="contained" sx={{width:"100%", minHeight:"500px"}}>
-              <Grid container>
-                <Grid item align="center" xs={12}>
-                  <AddIcon fontSize="large"></AddIcon>
+              </Card>
+            ) : (
+              <Button onClick={() => handleOpen(true)} variant="contained" sx={{ width: "100%", minHeight: "500px" }}>
+                <Grid container>
+                  <Grid item align="center" xs={12}>
+                    <AddIcon fontSize="large"></AddIcon>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Button>
-          )}
+              </Button>
+            )}
+          </Grid>
+          <Grid item xs={6}>
+            {/*Second Car image*/}
+            {secondCar ? (
+              <Card sx={{ width: "100%", height: "100%", textAlign: "center", position: "relative" }} onClick={() => handleOpen(false)}>
+                <Fab sx={{ position: "absolute", top: "25px", right: "25px" }}>
+                  <EditIcon fontSize="large" />
+                </Fab>
+                <CardActionArea>
+                  <CardContent>
+                    <Grid container spacing={3}>
+                      <Grid item xs={12}>
+                        <img
+                          src={process.env.PUBLIC_URL + secondCar.img}
+                          alt="First car"
+                          style={{
+                            objectFit: "cover",
+                            objectPosition: "center",
+                            width: "100%",
+                            height: "auto",
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={2} sm={2} md={2} lg={2}>
+                        <img
+                          src={getBrandImg(secondCar.brand)}
+                          alt="brand"
+                          style={{
+                            objectFit: "cover",
+                            objectPosition: "center",
+                            width: "80%",
+                            height: "auto",
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={5}>
+                        <Typography
+                          gutterBottom
+                          variant="h3"
+                          align="left"
+                          component="div"
+                        >
+                          {secondCar.name}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={5}>
+                        <Typography align="right" variant="h3">
+                          {secondCar.year}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography align="left" variant="h5">
+                          {secondCar.description}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ) : (
+              <Button onClick={() => handleOpen(false)} variant="contained" sx={{ width: "100%", minHeight: "500px" }}>
+                <Grid container>
+                  <Grid item align="center" xs={12}>
+                    <AddIcon fontSize="large"></AddIcon>
+                  </Grid>
+                </Grid>
+              </Button>
+            )}
+          </Grid>
+
+
+          <Grid item xs={6} >
+            {charList.map((it) => (
+              <DescriptionList
+                key={it}
+                title={it}
+                item={it.toLowerCase()}
+                car={firstCar}
+              />
+            ))}
+          </Grid>
+          <Grid item xs={6}>
+            {charList.map((it) => (
+              <DescriptionList
+                key={it}
+                title={it}
+                item={it.toLowerCase()}
+                car={secondCar}
+              />
+            ))}
+          </Grid>
+
         </Grid>
+        <CompareModal
+          open={open}
+          hClose={() => setOpen(false)}
+          hSelect={selection ? selectFirstCar : selectSecondCar}
+          cars={carList}
+        />
       </Grid>
-      <Grid item xs={6} >
-        {charList.map((it) => (
-          <DescriptionList
-            key={it}
-            title={it}
-            item={it.toLowerCase()}
-            car={firstCar}
-          />
-        ))}
-      </Grid>
-      <Grid item xs={6}>
-        {charList.map((it) => (
-          <DescriptionList
-            key={it}
-            title={it}
-            item={it.toLowerCase()}
-            car={secondCar}
-          />
-        ))}
-      </Grid>
-      <CompareModal
-        open={open}
-        hClose={() => setOpen(false)}
-        hSelect={selection ? selectFirstCar : selectSecondCar}
-        cars={carList}
-      />
-    </Grid>
+    </Container>
+
   );
 }
